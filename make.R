785c59a5c95a22de56697fcc2f5b3310d18aa5a5
Romain Frelat <rom.frelat@gmail.com>
2025-07-31 11:33:36 +0200

    structure for quarto presentations

---
+++ make.R
@@ -0,0 +1,13 @@
+# render the quarto presentations
+
+# github introduction
+# quarto::quarto_render("intro-git.qmd")
+
+# trait presentation
+# quarto::quarto_render("fellow-traits.qmd")
+
+# trait presentation
+quarto::quarto_render("env-data.qmd")
+
+# or in the terminal:
+#> quarto render
